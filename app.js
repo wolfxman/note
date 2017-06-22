1d6fa1b55255283f260be5283c2992975b90d1ca
wolf <925884565@qq.com>
2017-06-22 21:06:45 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var list = require('./routes/list');
+var admin = require('./routes/admin');
+var login = require('./routes/login');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views/pages'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', login);
+app.use('/users', users);
+app.use('/list', list);//list notes.
+app.use('/admin', admin);//write note.
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//create server
+app.listen(port);
+console.log('Server started on port ' + port);
+
+module.exports = app;
+
